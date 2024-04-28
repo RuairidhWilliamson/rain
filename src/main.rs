a31591c3ff1d26df15fb12d4782ad745a36da628
Ruairidh Williamson <williamsonruairidh@gmail.com>
2024-04-28 22:38:22 +0100

    Switch log env var to RAIN_LOG

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@ mod stdlib;
 use std::process::ExitCode;
 
 use clap::Parser;
+use tracing_subscriber::EnvFilter;
 
 fn main() -> ExitCode {
-    tracing_subscriber::fmt::init();
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_env("RAIN_LOG"))
+        .init();
 
     let cli = cmd::Cli::parse();
     cli.run()
